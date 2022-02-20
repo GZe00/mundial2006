7d6c74bd9070d31030adc97c25d516298e06b4da
Moises Fuentes <dev.gze.moises@gmail.com>
2022-02-20 12:06:49 -0600

    Bandera del pais

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -6,14 +6,13 @@ import { HiOutlineArrowCircleLeft } from '@react-icons/all-files/hi/HiOutlineArr
 import { navigate } from 'gatsby'
 
 const Country = ({ location }) => {
-  console.log(location)
-
   const [loc, setLoc] = React.useState()
   const [country, setCountry] = React.useState()
 
   React.useEffect(() => {
     getCountry(location.pathname.split("/")[2]).then(response => {
       setLoc(response.nombre)
+      setCountry(response)
       console.log(response)
 
     })
@@ -31,7 +30,15 @@ const Country = ({ location }) => {
           <HiOutlineArrowCircleLeft className='cursor-pointer text-gray-500' size={20} />
           <p className='cursor-pointer ml-2 mb-0 font-semibold text-gray-500 '>/Paises/Pais/{loc ?? null}</p>
         </div>
-        Hola
+        {
+          country ?
+            <div className='w-full' style={{ height: "100%" }}>
+              <div className='mx-auto' style={{ width: "480px", height: "256px" }}>
+                <img className='object-cover' src={`${country.bandera.formats.medium.url}`} alt={`Bandera de: ${country.bandera.alternativeText}`} />
+              </div>
+            </div>
+            : null
+        }
       </LayoutPage>
     </>
   )
