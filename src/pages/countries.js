70e0ec606b317d8b9ca6f426144d750a66f4cd82
Moises Fuentes <dev.gze.moises@gmail.com>
2022-02-20 11:38:34 -0600

    Lista de paises y reedirige a un pais

--- src/pages/countries.js
+++ src/pages/countries.js
@@ -1,20 +1,53 @@
 import React from 'react'
 import LayoutPage from '../components/LayoutPage'
-import ListCountries from '../components/ListCountries'
 import Seo from '../components/seo'
+import { getCountries } from "../api/content"
+import { List, Avatar } from 'antd';
+import { navigate } from 'gatsby';
 
-const countries = () => {
+const Countries = () => {
+
+  const [listCountries, setListCountries] = React.useState()
+
+  React.useEffect(() => {
+    getCountries().then(response => {
+      console.log(response)
+      setListCountries(response.sort((a, b) => (a.nombre > b.nombre) ? 1 : -1))
+      response.sort((a, b) => (a.nombre > b.nombre) ? 1 : -1)
+    })
+  }, []);
+
   return (
     <>
-    <Seo title="Paises" />
+      <Seo title="Paises" />
       <LayoutPage section="/countries">
-        <div className='flex w-full' style={{height:"100%"}}>
-          <div className='w-64 bg-orange-400 h-screen' >
+        <div className='flex flex-col md:flex-row w-full' style={{ height: "100%" }}>
+          <div className='w-64 md:mr-3 border h-screen overflow-y-auto md:pb-6 overflow-x-hidden' >
             <p className='mb-0'>Selecciona un pais</p>
             <p>para ver más detalles</p>
-            <ListCountries />
+
+            <List
+              loading={listCountries ? false : true}
+              itemLayout="horizontal"
+              dataSource={listCountries}
+              style={{ width: "128px", margin: "0 auto 0 auto" }}
+              renderItem={item => (
+                <List.Item>
+                  <List.Item.Meta
+                  
+                    className="hover:scale-110 ease-in duration-300 flex justify-center items-center"
+                    avatar={<Avatar className='cursor-pointer' src={item.bandera.formats.thumbnail.url} alt={item.bandera.alternativeText} />}
+                    title={<p onClick={() => navigate(`/country/${item.nombre}`)} className='cursor-pointer hover:text-blue-600 mb-0'>{item.nombre}</p>}
+                  />
+                </List.Item>
+              )}
+            />
+
+
+
           </div>
-          <div className='w-full bg-purple-700 h-screen' >
+
+          <div className='w-full md:ml-2 border h-screen' >
             <div>
               Carrousel posiblemente con todos los paises
             </div>
@@ -28,4 +61,4 @@ const countries = () => {
   )
 }
 
-export default countries
+export default Countries
