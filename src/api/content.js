09be9ead42a1d95be3cfb95e828eaa9eedc25fa5
Moises Fuentes <dev.gze.moises@gmail.com>
2022-02-20 11:37:29 -0600

    Servicios para pedir informacion de todos los paises y de cada pais en especifico

--- src/api/content.js
+++ src/api/content.js
@@ -1,5 +1,43 @@
 import config from "../config"
 
+const getCountries = () => {
+    return new Promise((resolve, reject) => {
+        fetch(`${config.server}/pais`).then(response => {
+            if (response && response.ok) {
+                response.json().then(data => {
+                    resolve(data)
+                })
+                    .catch(error => {
+                        console.log(error)
+                        reject(error)
+                    })
+            }
+        }).catch(error => {
+            console.log(error)
+            reject(error)
+        })
+    })
+}
+
+const getCountry = country => {
+    return new Promise((resolve, reject) => {
+        fetch(`${config.server}/pais?nombre=${country}`) .then(response => {
+            if(response.ok){
+                response.json().then(data => {
+                    resolve(data[0])
+                })
+                .catch(error => {
+                    console.log(error)
+                    reject(error)
+                })
+            }
+        }) .catch(error => {
+            console.log(error)
+            reject(error)
+        })
+    })
+}
+
 const getGroups = () => {
     return new Promise((resolve, reject) => {
         fetch(`${config.server}/grupos`).then(response => {
@@ -40,6 +78,7 @@ const getGroup = group => {
 
 export {
     getGroups,
-    getGroup
-
+    getGroup,
+    getCountries,
+    getCountry
 }
