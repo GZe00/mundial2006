cd18a4fe45076cd86914603c3e377db1e2d99d23
Moises Fuentes <dev.gze.moises@gmail.com>
2022-02-24 19:21:37 -0600

    Correcion en etiqueta static image

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -100,7 +100,7 @@ const Country = ({ location }) => {
 
               </div>
               <div className="w-2/5 flex justify-center items-center px-4" style={{ height: "100%" }}>
-                <div className="object-contain" style={{borderRadius: "15px"}}>
+                <div className="object-contain w-full" style={{height: "auto", borderRadius: "15px"}}>
                   <StaticImage src="../../images/test.jpg" alt="Prueba de test" />
                 </div>
               </div>
